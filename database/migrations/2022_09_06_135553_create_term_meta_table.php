080499f2c6e3a19ab3d291438112f633b6a9bbe2
imranshuvo <imrankhanshuvo@gmail.com>
2022-09-06 20:10:34 +0600

    Added database schema, added fonts, css library|

---
+++ database/migrations/2022_09_06_135553_create_term_meta_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('term_meta', function (Blueprint $table) {
+            $table->id();
+            $table->string('meta_key');
+            $table->string('meta_value');
+            $table->string('term_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('term_meta');
+    }
+};
