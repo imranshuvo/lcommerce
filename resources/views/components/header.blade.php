cc79207ec28427613e17ef2b2a348b0ac00da59e
imranshuvo <imrankhanshuvo@gmail.com>
2022-09-08 08:07:38 +0600

    Added breeze auth

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,9 +5,7 @@
             <div class="nav-wrapper">
                 <a href="#" class="brand-logo">{{ env('APP_NAME')}}</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="">Sass</a></li>
-                    <li><a href="">Components</a></li>
-                    <li><a href="">JavaScript</a></li>
+                    <li><a href="/login">Login</a></li>
                 </ul>
             </div>
         </div>
